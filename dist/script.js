06f5ee3fa5ed28e714d9f8278c5c0760c64f1489
nikay666 <lilit666an@gmail.com>
2020-09-13 22:46:14 +0300

    start slider

--- dist/script.js
+++ dist/script.js
@@ -2633,27 +2633,70 @@ const forms = state => {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+const classes = {
+  slider: '.slider',
+  contentWrap: '.slider__rooms-medias',
+  slidesWrap: 'wrap-medias',
+  img: '.slider__rooms-img',
+  description: '.slider__rooms-desc',
+  dots: '.slider-dots',
+  arrow: '.slider-arrows',
+  prevArrow: '.slider-arrow-l',
+  nextArrow: '.slider-arrow-r',
+  idRoom: 'rooms',
+  idReviews: 'reviews',
+  activeClass: 'active'
+};
+
 class Slider {
-  constructor(classes) {}
+  constructor(wrap) {
+    this.wrap = wrap;
+    this.slides = this.wrap.querySelectorAll(classes.contentWrap);
+    this.slidesWrap = this.wrap.querySelector(classes.slidesWrap);
+    this.dots = this.wrap.querySelectorAll(classes.dots);
+    this.prev = this.wrap.querySelector(classes.prevArrow);
+    this.next = this.wrap.querySelector(classes.nextArrow);
+    this.slideIndex = 1;
+    this.activeClass = classes.activeClass;
+  }
+
+  bindDots() {
+    this.dots.forEach((dot, i) => {
+      dot.addEventListener('click', e => {
+        console.log('Dot', e.target);
+      });
+    });
+  }
+
+  bindBtns() {
+    console.log(this.prev);
+    this.prev.addEventListener('click', e => {
+      console.log('prev', e.target);
+    });
+    this.next.addEventListener('click', e => {
+      console.log('next', e.target);
+    });
+  }
+
+  binding() {
+    this.bindBtns();
+    this.bindDots();
+  }
+
+  init() {
+    this.binding();
+  }
 
 }
 
 const gallery = () => {
-  const classes = {
-    slider: '.slider',
-    contentWrap: '.slider__rooms-medias',
-    img: '.slider__rooms-img',
-    description: '.slider__rooms-desc',
-    dots: '.slider-dots',
-    arrow: '.slider-arrows'
-  };
+  const wrapRooms = document.getElementById(classes.idRoom); // const wrapReviews = document.getElementById(classes.idReviews);
+
+  const sliderRoom = new Slider(wrapRooms);
+  sliderRoom.init();
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (gallery); //eq-clew.svg клубок(игровой комплекс)
-//eq-bed.svg лежак 
-//eq-home.svg домик
-//eq-sharpener.svg когтеточка
-//
+/* harmony default export */ __webpack_exports__["default"] = (gallery);
 
 /***/ }),
 
--- src/js/componenst/gallery.js
+++ src/js/componenst/gallery.js
@@ -1,31 +1,71 @@
-class Slider {
-    constructor(classes){
-
-    }
+const classes = {
+    slider: '.slider',
+    contentWrap: '.slider__rooms-medias',
+    slidesWrap: 'wrap-medias',
+    img: '.slider__rooms-img',
+    description: '.slider__rooms-desc',
+    dots: '.slider-dots',
+    arrow: '.slider-arrows',
+    prevArrow: '.slider-arrow-l',
+    nextArrow:  '.slider-arrow-r',
+    idRoom: 'rooms',
+    idReviews: 'reviews',
+    activeClass: 'active'
 }
 
 
+class Slider {
+    constructor(wrap){
+        this.wrap = wrap
+        this.slides = this.wrap.querySelectorAll(classes.contentWrap);
+        this.slidesWrap = this.wrap.querySelector(classes.slidesWrap)
+        this.dots = this.wrap.querySelectorAll(classes.dots);
+        this.prev = this.wrap.querySelector(classes.prevArrow);
+        this.next = this.wrap.querySelector(classes.nextArrow)
+        this.slideIndex = 1;
+        this.activeClass =  classes.activeClass
+    }
 
+    bindDots(){
+        this.dots.forEach((dot, i) => {
+            dot.addEventListener('click',(e) => {
+                console.log('Dot', e.target)
+              
+            })
+        });
+    }
 
+    bindBtns(){
+        console.log(this.prev)
+        this.prev.addEventListener('click', e => {
+            console.log('prev', e.target)
+        })
 
+        this.next.addEventListener('click', e => {
+            console.log('next', e.target)
+        })
+    }
 
+    binding(){
+        this.bindBtns()
+        this.bindDots()
+    }
+
+    init(){
+        this.binding()
+    }
+}
 
 const gallery = ()  => {
-    const classes = {
-        slider: '.slider',
-        contentWrap: '.slider__rooms-medias',
-        img: '.slider__rooms-img',
-        description: '.slider__rooms-desc',
-        dots: '.slider-dots',
-        arrow: '.slider-arrows'
+    const wrapRooms =  document.getElementById(classes.idRoom);
 
-    }
+    // const wrapReviews = document.getElementById(classes.idReviews);
+
+    const sliderRoom =  new Slider(wrapRooms);
+
+    sliderRoom.init();
+
 
 }
 
 export default gallery;
-//eq-clew.svg клубок(игровой комплекс)
-//eq-bed.svg лежак 
-//eq-home.svg домик
- //eq-sharpener.svg когтеточка
- //
