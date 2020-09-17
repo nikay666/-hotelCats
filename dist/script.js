9a4248149b1223a5bef36e31a6e6c82587468ca1
nikay666 <lilit666an@gmail.com>
2020-09-17 20:55:46 +0300

    Исправлен баг с шириной галерей

--- dist/script.js
+++ dist/script.js
@@ -2569,55 +2569,7 @@ function bottomFilter(type, json) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const forms = state => {
-  console.log('form'); //     const form = document.querySelectorAll('form'),
-  //             inputs = document.querySelectorAll('input');
-  //     checkNumInputs('input[name="user_phone"]');
-  //     const message = {
-  //         loading: 'Загрузка...',
-  //         success: 'Спасибо! Скоро мы с Вами свяжемся',
-  //         failure: 'Что-то пошло не так'
-  //     };
-  //     const postData = async (url, data) => {
-  //         document.querySelector('.status').textContent = message.loading;
-  //         let res = await fetch(url, {
-  //             method:"POST", 
-  //             body: data
-  //         });
-  //         return await res.text();
-  //     }   
-  //     const clearInpurs = () => {
-  //         inputs.forEach(input => {
-  //             input.value = '';
-  //         });
-  //     }
-  //     form.forEach(item => {
-  //         item.addEventListener('submit', (e) => {
-  //             e.preventDefault();
-  //             let statusMessage = document.createElement('div');
-  //             statusMessage.classList.add('status');
-  //             item.appendChild(statusMessage);
-  //             const formDate = new FormData(item);
-  //             if(item.getAttribute('data-calc') === 'end'){
-  //                 for (let key in state){
-  //                     formDate.append(key, state[key]);
-  //                 }
-  //             }
-  //             postData('assets/server.php', formDate)
-  //                 .then(res => {
-  //                     console.log(res);
-  //                     statusMessage.textContent = message.success;
-  //                 })
-  //                 .catch(() => {
-  //                     statusMessage.textContent = message.failure;
-  //                 })
-  //                 .finally(() => {
-  //                     clearInpurs();
-  //                     setTimeout(() => {
-  //                         statusMessage.remove();
-  //                     }, 5000 );
-  //                 })
-  //         });
-  //     });
+  console.log('form');
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (forms);
