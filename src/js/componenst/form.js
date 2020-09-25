fadc67760eff79d30601fadac063f9bcce0f9d58
Nika Yakubova <lilit666an@gmail.com>
2020-09-25 22:26:13 +0300

    Удаление лишнего form.js

--- src/js/componenst/form.js
+++ src/js/componenst/form.js
@@ -1,9 +1,5 @@
 const forms = (form = null) => {
     if(form  ===  null) return null; 
-    
-    // var f = document.forms.namedItem("booking");
-    // const f  =  document.querySelector('form')
-    // const formData = new FormData(f)
     console.log(form)    
 
 };
